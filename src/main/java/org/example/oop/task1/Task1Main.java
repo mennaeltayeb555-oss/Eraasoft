66e7419fd3ef4e90368fa91c5dd857cda108a92f
mennaeltayeb555-oss <mennaeltayeb555@gmail.com>
2026-02-15 21:41:10 +0200

    add-task-1

---
+++ src/main/java/org/example/oop/task1/Task1Main.java
@@ -0,0 +1,12 @@
+package org.example.oop.task1;
+
+public class Task1Main {
+    public static void main(String[] args) {
+        Student student = new Student(10,"menna",22);
+      
+    }
+
+
+}
+//Please Create App that accept id
+// , name and age of Student and print them (with oop)
